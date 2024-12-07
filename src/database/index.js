447b7cf24d90f1433554736d8001b138300742a7
Ramo s <ramodev.s155@gmail.com>
2024-12-07 21:16:40 +0000

    otimizacao na conexao com a db

--- src/database/index.js
+++ src/database/index.js
@@ -5,10 +5,7 @@ dotenv.config();
 
 export async function connectDatabase() {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log('Conectado ao MongoDB');
     } catch (error) {
         console.error('Erro ao conectar ao MongoDB:', error);
