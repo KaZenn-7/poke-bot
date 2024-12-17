1111bc088af391889c028adb467029d36f336e58
Ramo s <ramodev.s155@gmail.com>
2024-12-16 21:22:32 -0300

    getRandomPokemonByGeneration criado

---
+++ src/lib/pokemon/randomPokemonByGeneration.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export async function getRandomPokemonByGeneration(generation) {
+    try {
+  
+      let pokemonName;
+      let pokemonSpawnRate;
+      let randomSpawnRate = Math.floor(Math.random() * 255);
+  
+      do {
+  
+        let response = await axios.get(`https://pokeapi.co/api/v2/generation/${generation}`);
+        let randomPokemon = Math.floor(Math.random() * response.data.pokemon_species.length);
+  
+        pokemonName = response.data.pokemon_species[randomPokemon].name;
+  
+        response = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`);
+  
+        let pokemon_species = response.data;
+        pokemonSpawnRate = pokemon_species.capture_rate;
+  
+      } 
+      while (pokemonSpawnRate < randomSpawnRate);
+  
+      let response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+      return response.data;
+  
+    } catch (e) {
+      console.error(e);
+    }
+}
