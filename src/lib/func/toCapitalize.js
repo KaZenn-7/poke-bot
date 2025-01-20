bfa078bdac0196eb130708681f2606bfa41e06cf
Ramo s <ramodev.s155@gmail.com>
2025-01-20 20:19:47 +0000

    funcao para deixar a primeira letra das string em caixa alta

---
+++ src/lib/func/toCapitalize.js
@@ -0,0 +1,17 @@
+export function toCapitalize(ogString){
+    let string = ogString.toLowerCase();
+    let list = string.split(" ")
+    string = "";
+    for(let str of list){
+
+        let captalized = str[0].toUpperCase();
+        for (let i=1; i<str.length; i++){
+            captalized += str[i]
+        }
+        string += " "+captalized
+    }
+    
+    return string
+}
+
+console.log(toCapitalize("pikachu pei"));
